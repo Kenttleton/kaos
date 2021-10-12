f25cd2641c6ccd8131fbd527567b9f6b0b4c25f2
Kent Utterback <Kent.Utterback@cunamutual.com>
2021-10-12 09:21:44 -0700

    move to github

---
+++ README.md
@@ -0,0 +1,20 @@
+# Kenttleton OS
+
+Custom OS just for the fun of it.
+
+## To Build the OS
+
+`cargo build --target x86_64-kenttleton.json`
+
+## TODO
+
+[ ] Package Manager
+[ ] zsh like Shell
+[ ] Micro like text editor
+[ ] Filesystem navigation and management
+[ ] Implement stack unwinding to clear memory properly and not leave orphans
+
+### Optional
+
+[ ] Write custom bootloader
+[ ] Redo the Rust standard library for this OS
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// Look at different calling conventions 
+
+static HELLO: &str = "Hello, World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for(i, &byte) in HELLO.as_bytes().iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
